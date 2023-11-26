ec65b17c383bc5039cf535e7319af0dcde26fca6
Guo <1780465535@qq.com>
2023-11-26 22:29:15 +0800

    feat(Entry): remove tags from model Entry

--- book/models.py
+++ book/models.py
@@ -6,6 +6,7 @@ from users.models import MyUser
 
 # Create your models here.
 
+# 账本
 class Ledger(models.Model):
     user = models.ForeignKey(to=MyUser, on_delete=models.CASCADE)    
     title = models.CharField(max_length=20)
@@ -16,6 +17,7 @@ class Ledger(models.Model):
     def __str__(self):
         return self.title
 
+# 收支明细
 class Entry(models.Model):
     user = models.ForeignKey(to=MyUser, on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=20)
@@ -23,7 +25,6 @@ class Entry(models.Model):
     ledgers = models.ManyToManyField(to=Ledger, related_name='entries')
     category = models.IntegerField(default=0, blank=True) # 0: 其他, 1: 充值缴费, 2: 交通出行
     date_created = models.DateTimeField(default=timezone.now, blank=True)
-    tags = models.CharField(max_length=100, blank=True, null=True)   # 标签, 多个标签用逗号分隔
     notes = models.CharField(max_length=100, blank=True, null=True)  # 备注
     # TODO: 备注新建一个表，可以添加文字和图片
 
