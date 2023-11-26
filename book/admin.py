a33ebcc01f06d7172644fb3c804baae8929d3376
Guo <1780465535@qq.com>
2023-11-26 22:28:27 +0800

    feat(admin): LedgerAdmin & EntryAdmin more user-friendly administrator interface for ledger and entry

--- book/admin.py
+++ book/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
-
 from .models import Ledger, Entry
 
-admin.site.register(Ledger)
-admin.site.register(Entry)
+class LedgerAdmin(admin.ModelAdmin):
+    # list_display = ('id', 'title', 'icon', 'date_created', 'description')
+    list_display = ('id', 'title', 'icon', 'date_created', 'description', 'display_entries_id')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'description')
+    list_per_page = 25
+    
+    readonly_fields = ('display_id', 'display_entries') # 不能直接通过fields指定display_id
+    # 显示账本ID
+    def display_id(self, obj):
+        return obj.pk
+    display_id.short_description = 'ID'
+    # 显示账本下的所有明细，仅ID
+    def display_entries_id(self, obj):
+        return ", ".join([str(entry.pk) for entry in obj.entries.all()])
+    display_entries_id.short_description = 'Entries ID'
+    # 显示账本下的所有明细，包括ID和标题
+    def display_entries(self, obj):
+        return "\n".join([f"{entry.id}, {entry.title}" for entry in obj.entries.all()])
+    display_entries.short_description = 'Entries'
+
+    
+
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'amount', 'category', 'date_created', 'notes')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'notes')
+    list_per_page = 25
+
+
+admin.site.register(Ledger, LedgerAdmin)
+admin.site.register(Entry, EntryAdmin)
