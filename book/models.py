13e7b4b15c52e6836e18f46f47eb8644671aa889
Guo <1780465535@qq.com>
2023-11-27 20:48:22 +0800

    fix(Entry model): add field entry_type

--- book/models.py
+++ book/models.py
@@ -20,10 +20,16 @@ class Ledger(models.Model):
 # 收支明细
 class Entry(models.Model):
     user = models.ForeignKey(to=MyUser, on_delete=models.CASCADE, default=1)
+    ENTRY_TYPE = (
+        ('income', '收入'),
+        ('expense', '支出'),
+    )
+    entry_type = models.CharField(max_length=10, choices=ENTRY_TYPE, default='expense')
     title = models.CharField(max_length=20)
     amount = models.IntegerField(default=0)             # 金额 单位：分, 正入，负出
     ledgers = models.ManyToManyField(to=Ledger, related_name='entries')
     category = models.IntegerField(default=0, blank=True) # 0: 其他, 1: 充值缴费, 2: 交通出行
+    # TODO: category新建一个表, 以外键的形式关联
     date_created = models.DateTimeField(default=timezone.now, blank=True)
     notes = models.CharField(max_length=100, blank=True, null=True)  # 备注
     # TODO: 备注新建一个表，可以添加文字和图片
